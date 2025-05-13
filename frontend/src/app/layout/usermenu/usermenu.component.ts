cdd4b129d50576ff4c4029e78ca1dc255c636f98
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-05-13 20:24:22 +0200

    feat:creat layout, navbar, usermenu component feat:add in navbar.html navbar and login button

---
+++ frontend/src/app/layout/usermenu/usermenu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usermenu',
+  imports: [],
+  templateUrl: './usermenu.component.html',
+  styleUrl: './usermenu.component.css'
+})
+export class UsermenuComponent {
+
+}
