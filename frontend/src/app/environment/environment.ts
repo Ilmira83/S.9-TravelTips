65201ccc9ee789f9c34bdd9344978a6142025c60
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-05-30 15:12:18 +0200

    feat: create environment file

---
+++ frontend/src/app/environment/environment.ts
@@ -0,0 +1,10 @@
+export const environment  = {
+  production: false,
+  endpoint: 'http://localhost:3000/',
+  blogs: 'api/blogs/',
+  users: 'api/users/',
+  plans: 'api/plans/',
+  dailyPlans: 'api/daily-plans/',
+  dailyPlanByPlanID: 'by-plan/',
+  dailyPlanByBlogID: 'by-blog/',
+}
