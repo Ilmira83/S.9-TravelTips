2d868166fe3187c68f90901bee5b6353bfa737c1
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-05-30 15:35:38 +0200

    feat:update dependencies

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -15,13 +15,13 @@ export const routes: Routes = [
         },
       },
       {
-        path: 'app-profile',
+        path: 'app-profile/:uid',
         pathMatch: 'full',
         loadComponent: async () => {
-          const m = await import('./auth/profile/profile.component');
+          const m = await import('./user/profile/profile.component');
           return m.ProfileComponent;
         },
-      }, 
+      },
       {
         path: 'app-blogs-list',
         pathMatch: 'full',
@@ -31,6 +31,14 @@ export const routes: Routes = [
         },
       },
       {
+        path: 'app-about',
+        pathMatch: 'full',
+        loadComponent: async () => {
+          const m = await import('./about/about.component');
+          return m.AboutComponent;
+        },
+      }, 
+      {
         path: 'app-blog-editor',
         pathMatch: 'full',
         loadComponent: async () => {
@@ -38,7 +46,47 @@ export const routes: Routes = [
           return m.BlogEditorComponent;
         },
       },
-
+      {
+        path: 'app-blog-details/:id',
+        data: { breadcrumb: 'Blog details' },
+        pathMatch: 'full',
+        loadComponent: async () => {
+          const m = await import('./blogs/blog-details/blog-details.component');
+          return m.BlogDetailsComponent;
+        },
+      },
+      {
+        path: 'app-blogs',
+        pathMatch: 'full',
+        loadComponent: async () => {
+          const m = await import('./blogs/pages/blogs/blogs.component');
+          return m.BlogsComponent;
+        },
+      },
+      {
+        path: 'app-plan-list',
+        pathMatch: 'full',
+        loadComponent: async () => {
+          const m = await import('./plan/plan-list/plan-list.component');
+          return m.PlanListComponent;
+        },
+      },
+      {
+        path: 'app-plan-editor',
+        pathMatch: 'full',
+        loadComponent: async () => {
+          const m = await import('./plan/plan-editor/plan-editor.component');
+          return m.PlanEditorComponent;
+        },
+      },
+      {
+        path: 'app-plan-details/:id',
+        pathMatch: 'full',
+        loadComponent: async () => {
+          const m = await import('./plan/plan-details/plan-details.component');
+          return m.PlanDetailsComponent;
+        },
+      },
     ],
   },
 
