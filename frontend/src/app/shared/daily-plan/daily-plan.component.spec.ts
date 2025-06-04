a1650a28338eb419cc7ed53dad5c4184472b041b
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-06-04 12:30:38 +0200

    delete daily-plan-form.component.spec.ts

--- frontend/src/app/shared/daily-plan/daily-plan.component.spec.ts
+++ frontend/src/app/shared/daily-plan/daily-plan.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DailyPlanComponent } from './daily-plan.component';
 
@@ -21,3 +21,4 @@ describe('DailyPlanComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+ */
